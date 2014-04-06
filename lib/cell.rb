ce0f10a79e3b759303f843a59a92eb2edac8e4f8
Ankoor Patel <ankoor.p@gmail.com>
2014-04-06 18:30:16 +0100

    refactor cell and add world class to handle coordinates

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,18 +1,48 @@
 
 class Neighbourhood
-  attr_accessor :grid_size
+  attr_accessor :resident, :population
+
+  def initialize(resident: , population: )
+    @resident = resident
+    @population = population
+  end
 end
 
 class Cell
-  attr_accessor :coordinates, :state
+  attr_accessor :state
 
-  def initialize(a: 0, b: 0)
-    @coordinates = {a: a, b: b}
-    @state = 'alive'
+  def initialize(state: 'alive')
+    @state = state
   end
 
-  def spawn_at(a: 0, b: 0)
-    Cell.new(a: a, b: b)
+  def spawn
+    Cell.new(state: 'alive')
   end
 end
 
+class World
+  attr_accessor :cells, :neighbourhoods
+
+  def initialize(x:, y:)
+    @x, @y = x, y
+    populate
+    stitch_neighbourhoods @cells
+  end
+
+  def populate
+    @cells ||= []
+    @x.times do |x|
+      @y.times do |y|
+        cell = Cell.new
+        coordinates = [x, y]
+        @cells << { cell: cell, coordinates: coordinates }
+      end
+    end
+  end
+
+  def stitch_neighbourhoods(cells)
+    cells.each do |cell|
+
+    end
+  end
+end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,19 +1,46 @@
 require 'spec_helper'
 
 describe "game of life" do
-  context "cells" do
+  context "citizen" do
     subject { Cell.new }
 
     before(:each) do
-      @cell = subject.spawn_at(a: 3, b: 5)
+      @cell = subject.spawn
     end
 
-    it "spawns at the correct coodinates" do
-      @cell.coordinates.should == {a: 3, b: 5}
-    end
     it "is alive when spawned" do
       @cell.state.should == 'alive'
     end
-
   end
+
+  context "neighbourhoods" do
+    subject { Neighbourhood.new resident: "me", population: ["me", "you"] }
+
+    it "has the resident and population" do
+      @neighbourhood = subject
+      @neighbourhood.resident.should == "me"
+      @neighbourhood.population.should == ["me", "you"]
+    end
+  end
+
+  context "world" do
+      let(:x) { 5 }
+      let(:y) { 3 }
+
+    before(:each) do
+      @world = World.new x: x, y: y
+    end
+    it "has a citizen at every coordinate" do
+      x.times do |a|
+        y.times do |b|
+          expect(@world.cells.any? { |cell|
+            cell[:coordinates] == [a, b]
+          }).to be true
+        end
+      end
+    end
+  end
+
+
 end
+
